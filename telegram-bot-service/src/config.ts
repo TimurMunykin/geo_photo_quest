b2227183095a5c0ad7e4ef4b2bfb76126c51c5b9
Timur Munykin <timur.munykin@gmail.com>
2024-06-23 00:59:49 +0300

    fix api url in telegram service

--- telegram-bot-service/src/config.ts
+++ telegram-bot-service/src/config.ts
@@ -1,4 +1,4 @@
-export const HOST = 'https://767919-cf54261.tmweb.ru'
+export const HOST = 'https://767919-cf54261.tmweb.ru/api'
 export const BE_PORT = 3000
 // export const FULL_URL = `${HOST}:${BE_PORT}`
 export const FULL_URL = `${HOST}`
